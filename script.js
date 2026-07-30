00a643d1e5617bcd78db1b515ed3a45402333f5c
ElenaH-2026 <253562508+ElenaH-2026@users.noreply.github.com>
2026-07-30 21:17:54 +0200

    issue 4 - feat: Enhance question navigation and display question counter in quiz functionality

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function initQuiz(category) {
-    loadQuestion(category, questionIndex = 1);
-    loadAnswers(category, questionIndex = 1);
+    loadQuestion(category, questionIndex = 9);
+    loadAnswers(category, questionIndex = 9);
+    loadQuestionCounter(questionIndex = 9);
+    loadNumberOfQuestions(category);
 }  
 
 function loadQuestion(category, questionIndex) {
     const questionText = document.getElementById("#QuestionText");
     questionText.textContent = category[questionIndex].question;
-
 }
 
 function loadAnswers(category, questionIndex) {
@@ -25,3 +26,19 @@ function renderAnswerText(category, questionIndex, answerNr) {
     const answerText = document.getElementById("#AnswerText-" + answerNr);
     answerText.textContent = category[questionIndex].answers["answer_"  + answerNr].answer;
 }
+
+function loadQuestionCounter(questionIndex) {
+    const questionCounter = document.getElementById("#QuestionCounter");
+    questionCounter.textContent = questionIndex+1;
+}
+
+function loadNumberOfQuestions(category) {
+    const numberOfQuestions = document.getElementById("#NumberOfQuestions");
+    numberOfQuestions.textContent = category.length;
+}
+
+// function loadNextOrPreviousQuestion(category, questionIndex) {
+//     loadQuestion(category, questionIndex);
+//     loadAnswers(category, questionIndex);
+//     loadQuestionCounter(category, questionIndex);
+// }
